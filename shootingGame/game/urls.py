31b9ec4dd395e2f8d54748696c633327e0f9975d
idkwutocalmself <85773302+idkwutocalmself@users.noreply.github.com>
2023-01-01 17:13:41 -0800

    Delete shootingGame directory

--- shootingGame/game/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('main_page/', views.main_page),
-    path('singleplayer/', views.singleplayer),
-    path('multiplayer/', views.multiplayer)
-]
